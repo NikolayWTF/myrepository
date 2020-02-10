b609ff05c5c15b0c23943f005166183da14b532f
Николай Григорьев <kolan.gregorin01@gmail.com>
2020-02-10 12:28:48 +0300

    Добавил файл Apple.py

---
+++ Apple.py
@@ -0,0 +1,9 @@
+class Apple:
+    def __init__ (self, w, c, sort, size):
+        self.weight = w
+        self.color = c
+        self.sort = sort
+        self.size = size
+        print ("YES")
+
+Apple_1 = Apple (50, "red", "Antonovka", "medium")
